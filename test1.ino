b7a1ffc203fcba988fc9e20d02093252582f790b
dsxz19-2 <87105182+dsxz19-2@users.noreply.github.com>
2021-07-08 14:59:50 -0400

    Add files via upload

---
+++ test1.ino
@@ -0,0 +1,44 @@
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("iexplore https://i.imgur.com/8vVdzZD.jpg");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_A);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("%TEMP%\g.jpg");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(41, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("paint");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_O, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("%TEMP%\g.jpg");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("K");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("F");
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_F, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("x");
+  for(;;){ /*empty*/ }
+}
