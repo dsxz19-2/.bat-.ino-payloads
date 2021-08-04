48520b3b914ac8f7a724d6880a3be54723d21cb8
dsxz19-2 <87105182+dsxz19-2@users.noreply.github.com>
2021-08-03 20:09:38 -0400

    Delete mouse.py

--- mouse.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-time.sleep(1)
-pyautogui.hotkey("win","r")
-time.sleep(0.3)
-pyautogui.typewrite("RUNDLL32 USER32.DLL,SwapMouseButton", interval=0.02)
-time.sleep(0.2)
-pyautogui.hotkey("enter")
